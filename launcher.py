f4542845275e77005671c9a12c9700974447069d
Naveen Arivazhagan <arivazh2@illinois.edu>
2016-10-03 18:26:47 -0700

    arg fix

--- launcher.py
+++ launcher.py
@@ -48,7 +48,7 @@ values_ranges = {
     "decay_patience": [0],
     "total_epochs": [100],
     "test_only": [False],
-    "pre_trained": [None],
+    "pre_trained": [''],
     "train_size": [4000000],
     "test_size": [100000],
     "seed": [0],
